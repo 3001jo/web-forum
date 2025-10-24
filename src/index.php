d6a6ef7f3ed4d514bcd94c7dc8889865564becc5
3001jo <3001joooo@proton.me>
2025-10-25 01:24:45 +0200

    show author usernames

--- src/index.php
+++ src/index.php
@@ -13,13 +13,22 @@
 		echo '<br><a href="/admin.php">Admin Panel</a>';
 	}
 	$db = new SQLite3('../forum.db');
-	$result = $db->query('SELECT * FROM posts WHERE deleted = FALSE ORDER BY created desc');
+	$result = $db->query('
+		SELECT posts.*, users.*
+		FROM posts 
+		JOIN users ON posts.authorID = users.id 
+		WHERE posts.deleted = FALSE 
+		ORDER BY posts.created DESC
+	');
 	while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
 		echo '<h4>';
 		if ($_SESSION['admin'] == true) {
 			echo '<a href="/admin.php?delete_post=' . $row['id'] . '">Delete</a> ';
 		}
-		echo '> ' . htmlspecialchars($row['title']) . '</h4>' . nl2br(htmlspecialchars($row['text']));
+		if ($row['admin'] == true) {
+			echo '@';
+		}
+		echo '@' . htmlspecialchars($row['username']) . ' > ' . htmlspecialchars($row['title']) . '</h4>' . nl2br(htmlspecialchars($row['text']));
 	}
 	$db->close();
 	require '_footer.php';
